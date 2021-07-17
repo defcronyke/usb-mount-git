77f2a4477e19b21ef3990d6a060366aea96db27d
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-17 15:07:23 -0400

    Minor fixes.

--- usb-mount-git-symlinks.sh
+++ usb-mount-git-symlinks.sh
@@ -3,5 +3,9 @@
 cd /opt/git; for i in `ls -1d /media/*/*.git`; do ln -s "$i" 2>/dev/null || true; done
 
 cd /opt/git
+
+sudo chown -R pi: /home/pi/git/.git/gitweb
+sudo chown pi: /home/pi/git/.git/pid
+
 sudo su -s /bin/bash -c "GIT_DISCOVERY_ACROSS_FILESYSTEM=1 git instaweb --restart" -g pi pi
 
