6b210d92f36e92f33cf2f323c625273b622cb970
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-17 14:34:07 -0400

    Restart git instaweb periodically.

--- usb-mount-git-symlinks.sh
+++ usb-mount-git-symlinks.sh
@@ -2,5 +2,6 @@
 
 cd /opt/git; for i in `ls -1d /media/*/*.git`; do ln -s "$i" 2>/dev/null || true; done
 
+cd `ls -1d /opt/git/new/*.git | head -n 1`
 GIT_DISCOVERY_ACROSS_FILESYSTEM=1 git instaweb --restart
 
