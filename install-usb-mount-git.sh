6157e5e462ec55427151c172bbf2274332ef8ccd
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-22 01:44:23 -0400

    Improve scripts.

--- install-usb-mount-git.sh
+++ install-usb-mount-git.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-sudo mkdir -p /opt/git
-sudo chown $USER: /opt/git
-if [ ! -d "$HOME/git" ]; then
-	ln -s /opt/git $HOME/git 2>/dev/null
-fi
-sudo cp -f usb-mount-git@.service /etc/systemd/system/
-sudo cp -f 99-usb-mount-git.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules && sudo udevadm trigger
-sudo systemctl daemon-reload
+install_usb_mount_git() {
+  sudo mkdir -p /opt/git
+  sudo chown $USER: /opt/git
+  if [ ! -d "$HOME/git" ]; then
+    ln -s /opt/git $HOME/git 2>/dev/null
+  fi
+  sudo cp -f usb-mount-git@.service /etc/systemd/system/
+  sudo cp -f 99-usb-mount-git.rules /etc/udev/rules.d/
+  sudo udevadm control --reload-rules && sudo udevadm trigger
+  sudo systemctl daemon-reload
 
-sudo cp -f usb-mount-git-cron /etc/cron.d/
+  sudo cp -f usb-mount-git-cron /etc/cron.d/
+}
 
+install_usb_mount_git
--- usb-mount-git-symlinks.sh
+++ usb-mount-git-symlinks.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 
-cd /opt/git
+usb_mount_git_symlinks() {
+  cd /opt/git
 
-# Force-unmount dead mountpoints found in /media
-echo "Checking if any mount points under /media are dead or inaccessible..."
+  # Force-unmount dead mountpoints found in /media
+  echo "Checking if any mount points under /media are dead or inaccessible..."
 
-for i in `lsblk | awk '{print $7}' | tail -n +2 | grep "/media/"`; do
-  ls "$i"
-
-  if [ $? -ne 0 ]; then
-    echo "Broken or inaccessible mount point detected in /media, so we're force-unmounting it in case it's stuck: $i" && \
-    sudo umount -f "$i" && \
-    echo "Attempting to re-mount it once, but if it fails we won't try again." && \
-    sudo mount "`mount | grep "$i" | awk '{print $1}'`" "$i"
+  for i in `lsblk | awk '{print $7}' | tail -n +2 | grep "/media/"`; do
+    ls "$i"
 
     if [ $? -ne 0 ]; then
-      echo "Re-mounting dead or inaccessible mount point failed: $i"
+      echo "Broken or inaccessible mount point detected in /media, so we're force-unmounting it in case it's stuck: $i" && \
+      sudo umount -f "$i" && \
+      echo "Attempting to re-mount it once, but if it fails we won't try again." && \
+      sudo mount "`mount | grep "$i" | awk '{print $1}'`" "$i"
+
+      if [ $? -ne 0 ]; then
+        echo "Re-mounting dead or inaccessible mount point failed: $i"
+      fi
     fi
-  fi
-done
+  done
 
-# Add symlinks for all "*.git/" folders on usb disks to /opt/git/
-for i in `find /media -type d -name "*.git" 2>/dev/null | sed 's/\/.git$//g'`; do ln -s "$i" 2>/dev/null || true; done
+  # Add symlinks for all "*.git/" folders on usb disks to /opt/git/
+  for i in `find /media -type d -name "*.git" 2>/dev/null | sed 's/\/.git$//g'`; do ln -s "$i" 2>/dev/null || true; done
 
-# Add symlinks for all folders under /media/ to /opt/git/
-sudo ln -s /media /opt/git/media 2>/dev/null || true
+  # Add symlinks for all folders under /media/ to /opt/git/
+  sudo ln -s /media /opt/git/media 2>/dev/null || true
 
-for i in `ls -1ad /media/*`; do
-	sudo ln -s "$i" 2>/dev/null || true
-done
+  for i in `ls -1ad /media/*`; do
+    sudo ln -s "$i" 2>/dev/null || true
+  done
 
-# Update symlink for /opt/git/new/ to point to usb disk with most free space available.
-unlink /opt/git/new; ln -s `df | grep /media/ | sort -nrk4 | head -n1 | awk '{print $NF}'` /opt/git/new 2>/dev/null; ls /opt/git/new || unlink /opt/git/new; ln -s /media/local /opt/git/new 2>/dev/null || true
+  # Update symlink for /opt/git/new/ to point to usb disk with most free space available.
+  unlink /opt/git/new; ln -s `df | grep /media/ | sort -nrk4 | head -n1 | awk '{print $NF}'` /opt/git/new 2>/dev/null; ls /opt/git/new || unlink /opt/git/new; ln -s /media/local /opt/git/new 2>/dev/null || true
 
-sudo chown -R pi: /home/pi/git/.git/gitweb
-sudo chown pi: /home/pi/git/.git/pid
+  sudo chown -R pi: /home/pi/git/.git/gitweb
+  sudo chown pi: /home/pi/git/.git/pid
 
-sudo su -s /bin/bash -c "git instaweb --restart" -g pi pi
+  sudo su -s /bin/bash -c "git instaweb --restart" -g pi pi
+}
+
+usb_mount_git_symlinks
