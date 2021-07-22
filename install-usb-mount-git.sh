a4c8f09598d1e5683540ced8e1e48d4661a21187
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-22 07:12:22 -0400

    Support any user account.

--- install-usb-mount-git.sh
+++ install-usb-mount-git.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
 install_usb_mount_git() {
+  sudo groupadd $USER 2>/dev/null
+
   sudo mkdir -p /opt/git
   sudo chown $USER: /opt/git
   if [ ! -d "$HOME/git" ]; then
     ln -s /opt/git $HOME/git 2>/dev/null
   fi
-  sudo cp -f usb-mount-git@.service /etc/systemd/system/
+
+  cat usb-mount-git@.service.tmpl | \
+  sed "s/{USER}/$USER/g" | \
+  tee /etc/systemd/system/usb-mount-git@.service
+
   sudo cp -f 99-usb-mount-git.rules /etc/udev/rules.d/
   sudo udevadm control --reload-rules && sudo udevadm trigger
   sudo systemctl daemon-reload
 
-  sudo cp -f usb-mount-git-cron /etc/cron.d/
+  sudo cp -f usb-mount-git-symlinks.sh /etc/cron.d/
+  
+  cat usb-mount-git-cron.tmpl | \
+  sed "s/{USER}/$USER/g" | \
+  sudo tee /etc/cron.d/usb-mount-git-cron
 }
 
 install_usb_mount_git
--- usb-mount-git-symlinks.sh
+++ usb-mount-git-symlinks.sh
@@ -36,10 +36,10 @@ usb_mount_git_symlinks() {
   # Update symlink for /opt/git/new/ to point to usb disk with most free space available.
   unlink /opt/git/new; ln -s `df | grep /media/ | sort -nrk4 | head -n1 | awk '{print $NF}'` /opt/git/new 2>/dev/null; ls /opt/git/new || unlink /opt/git/new; ln -s /media/local /opt/git/new 2>/dev/null || true
 
-  sudo chown -R pi: /home/pi/git/.git/gitweb
-  sudo chown pi: /home/pi/git/.git/pid
+  sudo chown -R $USER: /home/$USER/git/.git/gitweb
+  sudo chown $USER: /home/$USER/git/.git/pid
 
-  sudo su -s /bin/bash -c "git instaweb --restart" -g pi pi
+  sudo su -s /bin/bash -c "git instaweb --restart" -g $USER $USER
 }
 
 usb_mount_git_symlinks
